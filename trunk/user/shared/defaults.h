2e3e512a6f2880595d24273987d61170d834025d
Yonsm <Yonsm@qq.com>
2019-09-17 22:23:07 +0800

    Yonsm's default config

--- trunk/user/shared/defaults.h
+++ trunk/user/shared/defaults.h
@@ -30,9 +30,14 @@
 #define SYS_USER_NOBODY		"nobody"
 #define SYS_GROUP_NOGROUP	"nogroup"
 
-#define DEF_LAN_ADDR		"192.168.2.1"
-#define DEF_LAN_DHCP_BEG	"192.168.2.100"
-#define DEF_LAN_DHCP_END	"192.168.2.244"
+#if defined(BOARD_ROLE_REPEATER)
+#define DEF_LAN_ADDR		"192.168.1.254"
+#else
+#define DEF_LAN_ADDR		"192.168.1.1"
+#endif
+#define DEF_LAN_GATEWAY		"192.168.1.1"
+#define DEF_LAN_DHCP_BEG	"192.168.1.100"
+#define DEF_LAN_DHCP_END	"192.168.1.200"
 #define DEF_LAN_MASK		"255.255.255.0"
 
 #define DEF_WLAN_2G_CC		"CN"
@@ -44,6 +49,7 @@
 #define DEF_WLAN_2G_PSK		"1234567890"
 #define DEF_WLAN_5G_PSK		"1234567890"
 
+#define DEF_DEF_COMPUTER_NAME   "Router" //BOARD_NAME
 #define DEF_ROOT_PASSWORD	"admin"
 #define DEF_SMB_WORKGROUP	"WORKGROUP"
 #define DEF_TIMEZONE		"CST-8"
