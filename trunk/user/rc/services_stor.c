642eb60f102957c937a18855f2da5c26568d3fe2
Yonsm <Yonsm@qq.com>
2019-12-15 17:55:53 +0800

    Veto ._* files

--- trunk/user/rc/services_stor.c
+++ trunk/user/rc/services_stor.c
@@ -270,7 +270,7 @@ write_smb_conf(void)
 	fprintf(fp, "dos filetimes = yes\n");
 	fprintf(fp, "dos filetime resolution = yes\n");
 	fprintf(fp, "access based share enum = yes\n");
-	fprintf(fp, "veto files = /Thumbs.db/.DS_Store/._.DS_Store/.apdisk/.TemporaryItems/");
+	fprintf(fp, "veto files = /Thumbs.db/.DS_Store/._*/.apdisk/.TemporaryItems/");
 	fprintf(fp, "\n");
 
 	disks_info = read_disk_data();
